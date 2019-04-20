27a4f8b849e5de30826f00ab60f3652fa7adc903
KaoruOta <30927817+KaoruOta@users.noreply.github.com>
2019-04-21 00:27:46 +0900

    make yawrate_ctl class
    make yawrate_ctl class

--- Codes/Operation/operation.hpp
+++ Codes/Operation/operation.hpp
@@ -9,6 +9,7 @@
 #include "parameter.h"
 #include "GyroSensor.h"
 #include "Motor.h"
+#include "yawrate_ctl.hpp"
 
 
 using namespace std;
@@ -41,6 +42,7 @@ private:
   ev3api::Motor& mTail_Motor;
 
   PID *gForward  = new PID();
+  Yawrate_Ctl *gYawrate_Ctl = new Yawrate_Ctl();
 
   enum Robo_Mode{
     SET,
@@ -70,60 +72,6 @@ private:
   int right_wheel_enc        = 0;
   int left_wheel_enc         = 0;
 
-
-  float YawrateController(float yawrate, float yawrate_cmd);
-  float yaw_ctl_dt = 0.004;
-
-  float turn_tmp   = 0.0;
-  float r_yaw_rate = 0.0;
-  float r_yaw_rate_ud = 0.0;
-  float I_gain1 = 1.0+1.0/0.8;
-  float I_gain2 = yaw_ctl_dt/0.06/0.8;
-  float I_gain3 = 1.0-yaw_ctl_dt/0.06;
-
-  float F_controller(float r_yaw_rate);
-  float F_in = 0.0;
-  float F_out = 0.0;
-  //    float F_gain = 1/0.062;
-  float F_gain = 1/0.062; //0818 tada
-    
-  float E_controller(float r_yaw_rate);
-  float E_in;
-  float E_in_d;
-  float E_in_dd;
-  float E_in_ddd;
-  float E_in_dddd;
-  float E_in_ddddd = 0.0;
-  float E_in_dddddd = 0.0;
-
-  float E_out = 0.0;
-  float E_gain1 = yaw_ctl_dt/0.1;
-  float E_gain2 = 1.0-yaw_ctl_dt/0.1*1.0;
-  float E_ud1 = 0.0;
-
-  float C_controller(float E_out, float yawrate, float S_out);
-  float C_in = 0.0;
-  float C_out = 0.0;
-  //float C_gain = yaw_ctl_dt*10.0;
-  float C_gain = yaw_ctl_dt*50.0;
-  float C_ud1 = 0.0;
-
-  float S_controller(float C_out);
-  float S_gain1 = 1.0;
-  float S_in;
-  float S_in_d;
-  float S_in_dd;
-  float S_in_ddd;
-  float S_in_dddd;
-  float S_in_ddddd = 0.0;
-  float S_in_dddddd = 0.0;
-  float S_out = 0.0;
-  float Pn_gain1 = yaw_ctl_dt/0.1;
-  float Pn_gain2 = 1.0-yaw_ctl_dt/0.1*1.0;
-  float Pn_ud1 = 0.0;
-  float Pd_gain1 = yaw_ctl_dt/0.1;
-  float Pd_gain2 = 1.0-yaw_ctl_dt/0.1*1.0;
-  float Pd_ud1 = 0.0;
 
   void PWM_Gen(int mForward, float mTurn);
   int left_motor_pwm;
